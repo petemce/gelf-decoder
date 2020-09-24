3b128f54ca38639a677ad12b07bc09074c47fdc7
petemce <37329061+petemce@users.noreply.github.com>
2020-09-24 19:24:28 +0100

    Create gelf.py

---
+++ gelf.py
@@ -0,0 +1,18 @@
+#!//usr/bin/python
+"""This will decode zlib compressed gelf, for gzip compressed
+   use gzip and stringio
+"""
+
+import pyshark
+import zlib
+import binascii
+import json
+import pprint
+
+cap = pyshark.LiveCapture('ens192', bpf_filter='port 12201')
+
+for packet in cap.sniff_continuously(packet_count=1000):
+    try:
+        pprint.pprint(json.loads(zlib.decompress(binascii.unhexlify(packet.data.data))))
+    except zlib.error:
+        continue
